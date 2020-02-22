57a3439bb5db2a644545172b55095e84f8f3d432
jonschacter <schacter.jon@gmail.com>
2020-02-22 14:05:21 -0800

    Done.

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class MP3Importer
+    attr_accessor :path
+
+    def initialize(path)
+        @path = path
+    end
+
+    def files
+        files = Dir.glob("#{@path}/*.mp3").collect { |file| file.gsub("#{@path}/","") }
+    end
+
+    def import
+        self.files.each{ |file| Song.new_by_filename(file) }
+    end
+end
+
+file_path = "./spec/fixtures/mp3s"
+m = MP3Importer.new(file_path)
+
+#binding.pry
+
+#true
